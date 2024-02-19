9067053b322f949357d340e7fc777a5031474b1b
Abdelrahman Mahmoud <60822166+abdelrahmman97@users.noreply.github.com>
2024-02-19 07:48:24 +0000

    fixed importing of modules and components

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UnderConstructionComponent } from './components/under-construction/under-construction.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-    { path: '', component: UnderConstructionComponent }
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent }
 ];
 
 @NgModule( {
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
-import { UnderConstructionComponent } from './components/under-construction/under-construction.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule( {
     declarations: [
-        UnderConstructionComponent
+        HomeComponent,
+        ProjectsComponent
     ],
     imports: [
         CommonModule,
-        FeaturesRoutingModule
+        FeaturesRoutingModule,
+        SharedModule
     ]
 } )
 export class FeaturesModule { }
