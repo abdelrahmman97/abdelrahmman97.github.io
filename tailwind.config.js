618868eec94d0031195ce2858581a40d8af73c80
abdelrahmman97 <mabdelrahmman22@gmail.com>
2025-05-04 02:40:27 +0300

    chore: update Angular dependencies to version 17.3.12 and add new packages; modify Tailwind CSS configuration to extend screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,10 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			colors: {
-				'PRIMARY_DARKEST': '#170F2E',
-				'PRIMARY_DARKER': '#312163',
-				'PRIMARY_DARK': '#5531A7',
-				'PRIMARY_DEFAULT': '#9251F7',
-				'PRIMARY_LIGHT': '#C696FC',
-				'PRIMARY_LIGHTER': '#EFE2F9',
-				'PRIMARY_LIGHTEST': '#F8F2FC',
-				'SECONDARY_LIGHT': '#95AAFB',
-				'SECONDARY_LIGHTER': '#E1E7FE',
-				'SECONDARY_LIGHTEST': '#F5F7FF',
-				'GRAY_DARKER': '#2E2E48',
-				'GRAY_DEFAULT': '#79819A',
-			},
+			screens:{
+				'2xl': '1530px',
+				'3xl': '1800px'
+			}
 		},
 	},
 	plugins: [],
