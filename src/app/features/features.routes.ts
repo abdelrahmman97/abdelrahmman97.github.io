7f92283c68c527197f03d703c0791224a713135f
abdelrahmman97 <mabdelrahmman22@gmail.com>
2025-05-04 02:53:16 +0300

    feat: update pages routes to include home, about-me, projects, and contact components

--- src/app/features/features.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@angular/router";
-
-const featuresRoutes: Routes = [
-	{
-		path: '',
-		loadComponent: () => import( './components/under-construction/under-construction.component' ).then( m => m.UnderConstructionComponent )
-	}
-];
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -3,6 +3,18 @@ import { Routes } from '@angular/router';
 export const pagesRoutes: Routes = [
 	{
 		path: '',
-		loadComponent: () => import( './components/landing/landing.component' ).then( m => m.LandingComponent )
-	}
+		loadComponent: () => import( './components/home/home.component' ).then( m => m.HomeComponent )
+	},
+	{
+		path: 'about-me',
+		loadComponent: () => import( './components/about-me/about-me.component' ).then( m => m.AboutMeComponent )
+	},
+	{
+		path: 'projects',
+		loadComponent: () => import( './components/projects/projects.component' ).then( m => m.ProjectsComponent )
+	},
+	{
+		path: 'contact',
+		loadComponent: () => import( './components/contact-me/contact-me.component' ).then( m => m.ContactMeComponent )
+	},
 ]
